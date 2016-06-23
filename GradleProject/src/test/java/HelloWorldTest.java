77bbcaa33a3790ecb206b3b2383e460fb08fa63e
Tiwari <subhash.tiwari@capgemini.com>
2016-06-23 16:34:45 +0530

    Gradle script project

---
+++ GradleProject/src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+public class HelloWorldTest {
+	
+	@Test
+	public void tsetSuccessfully() {
+				
+		assertEquals(true, true);
+	}
+
+}
